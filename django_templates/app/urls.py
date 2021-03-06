8384c86f299262c88b61b2fdddc95438f8893d17
unknown <isaiasorton@gmail.com>
2021-03-06 13:29:05 -0300

    entendendo e criando rotas no django

---
+++ django_templates/app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path('home', index, name="home"),
+    path('contact/<id>', contact, name="contact"),
+    path('about', about, name="about"),
+]
--- django_templates/app/views.py
+++ django_templates/app/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    nome_empresa = "OrtonSec"
+    descricao_empresa = "Conteúdos a nível engenharia"
+
+    contato_empresa = {
+        "endereco": "Rua Cel. Rangel",
+        "telefone": "99999-9999",
+        "email": "isaiasorton@gmail.com"
+    }
+
+    # redenriza a page index.html passando variáveis como parâmetro para serem usadas na page
+    return render(request, 'index.html', {'nome_empresa': nome_empresa, 'descricao_empresa': descricao_empresa, 'contato_empresa': contato_empresa})
+
+
+def contact(request, id=5):
+    return HttpResponse("pagina contatos", id)
+
+
+def about(request):
+    return HttpResponse("Pagina sobre")
